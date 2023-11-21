f749db73739118ac5ed6d00afbdc3d3af9c4e4fe
Tanmay Seth <kushal.seth2010@gmail.com>
2023-11-21 19:35:41 +0530

    Changes related to analytics redirection

---
+++ QRClient/Controllers/AnalyticsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QRClient.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        // GET: AnalyticsController
+        public void Index()
+        {
+
+           // TODO: Make sure Analytics api running locally
+           Response.Redirect("http://127.0.0.1:5000/dash/");
+        }
+    }
+}
